37b63c1f6aed8eab4e29c89524806f64234f1b86
DJprogre33 <smirnovniki119@gmail.com>
2023-08-15 15:39:55 +0300

    contains_duplicate finished

---
+++ algorithms/arrays_and_hashing/contains_duplicate.py
@@ -0,0 +1,27 @@
+class Solution:
+    """link to the problem: https://leetcode.com/problems/contains-duplicate/"""
+    @staticmethod
+    def v1_contains_duplicate(nums: list[int]) -> bool:
+        """
+        Time complexity: O(n) 488ms beats 80.11%
+        Space complexity: 0(n) 31MB beats 43.7%
+        """
+        hashmap = set()
+        for n in nums:
+            if n in hashmap:
+                return True
+            hashmap.add(n)
+        return False
+
+    @staticmethod
+    def v2_contains_duplicate(nums: list[int]) -> bool:
+        """
+        Time complexity: O(n) 480ms beats 84.61%
+        Space complexity: 0(n) 34MB beats 6.75%
+        """
+        hashmap = {}
+        for n in nums:
+            hashmap[n] = hashmap.get(n, 0) + 1
+            if hashmap[n] > 1:
+                return True
+        return False
