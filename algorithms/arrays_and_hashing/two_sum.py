935a19c0afff07c2249eaf38a212b738c42164e0
DJprogre33 <smirnovniki119@gmail.com>
2023-08-15 16:24:02 +0300

    two_sum finished

---
+++ algorithms/arrays_and_hashing/two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    """link to the problem: https://leetcode.com/problems/two-sum/"""
+    @staticmethod
+    def v1_two_sum(nums: list, target: int) -> list[int]:
+        """
+        Time complexity: O(n) 62ms beats 91.45%
+        Space complexity: 0(n) 17.8ms beats 14.76%
+        """
+        hashmap = {}
+        for i, v in enumerate(nums):
+            if v in hashmap:
+                return [hashmap[v], i]
+            hashmap[target - v] = i
