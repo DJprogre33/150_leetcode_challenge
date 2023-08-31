a86e0ee486a3f6946ad4b8754deeb2963630af5a
DJprogre33 <smirnovniki119@gmail.com>
2023-08-31 13:19:40 +0300

    time_based_key_value_store.py done

---
+++ algorithms/binary_search/time_based_key_value_store.py
@@ -0,0 +1,41 @@
+class TimeMap:
+    """link to the problem: https://leetcode.com/problems/time-based-key-value-store/"""
+
+    # Time complexity: O(logN) 715ms beats 49.20%
+    # Space complexity: 0(n) 73.88mb beats 80.42%
+
+    def __init__(self):
+        self.hashmap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hashmap.setdefault(key, []).append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        result = ""
+        values = self.hashmap.get(key)
+        if values:
+            left, right = 0, len(values) - 1
+
+            while left <= right:
+                mid = left + (right - left) // 2
+
+                if values[mid][-1] == timestamp:
+                    result = values[mid][0]
+                    break
+
+                if values[mid][-1] < timestamp:
+                    result = values[mid][0]
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return result
+
+
+s = TimeMap()
+s.set("foo", "bar", 1)
+print(s.get("foo", 1))
+print(s.get("foo", 3))
+s.set("foo", "bar2", 4)
+print(s.get("foo", 4))
+print(s.get("foo", 5))
+
