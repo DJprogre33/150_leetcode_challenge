06f9d8328f5b085ff483b35ccce330ef0c721ecb
DJprogre33 <smirnovniki119@gmail.com>
2023-09-15 18:34:42 +0300

    add_two_numbers.py done

---
+++ algorithms/linked_list/add_two_numbers.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    """link to the problem: https://leetcode.com/problems/add-two-numbers/"""
+    @staticmethod
+    def v1_add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Time complexity: O(n) 65ms beats 63.36%
+        Space complexity: O(1) 16.21mb beats 90.60%
+        """
+        dummy = ListNode()
+        curr = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+
+            curr.next = ListNode(val)
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
